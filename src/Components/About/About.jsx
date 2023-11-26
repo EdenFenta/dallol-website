ab25044f7d5080621ad523a7cbcf9901d0286e05
Ruth99G <atsede99@yahoo.com>
2023-11-26 19:18:24 +0300

    Catalog component created

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,40 +3,64 @@ import "./About.css";
 
 const About = () => {
   return (
-    <div id="about" className="about">
+    <div id="/about" className="about">
       <h1>About Dallol</h1>
-      <h2>Inspiration</h2>
-      <p>
-        Dallol, a furniture company based in Ethiopia, is inspired by the
-        stunningly beautiful yet harsh Dallol Depression in Northern Ethiopia.
-        This location, known for its unique colors and textures, serves as the
-        muse for our designs. Our mission is to create a brand identity that is
-        both unique and memorable. We aim to reflect our company's values and
-        mission while also highlighting the connection to the location's colors
-        and textures.
-      </p>
-      <p>
-        To create a successful branding strategy, we consider several elements.
-        We strive to understand our target audience and what they value. This
-        helps us create a brand identity that resonates with them. We identify
-        and communicate our unique selling proposition effectively. Finally, we
-        ensure that our brand is consistent across all channels, including
-        social media, website, advertising, and packaging.
-      </p>
-      <h2>Identity</h2>
-      <p>
-        Our brand identity evokes a sense of authenticity and simplicity while
-        highlighting the connection to the earthy colors of the Dallol
-        Depression. Our logo is simple yet memorable, with a color palette that
-        reflects the location's distinct hues. Our tagline, "Elevate your space
-        with our earth-inspired designs," communicates our brand's mission and
-        values while also highlighting its connection to the location.
-      </p>
-      <p>
-        By incorporating these elements into our branding strategy, Dallol
-        differentiates itself from competitors and attracts customers who value
-        authenticity, durability, and unique design.
-      </p>
+      <div className="content">
+        <div className="section">
+          <h2>Inspiration</h2>
+          <p>
+            <span className="highlight">Dallol</span>, a furniture company based
+            in Ethiopia, is inspired by the
+            <span className="highlight">
+              {" "}
+              stunningly beautiful yet harsh Dallol Depression
+            </span>{" "}
+            in Northern Ethiopia. This location, known for its{" "}
+            <span className="highlight">unique colors and textures</span>,
+            serves as the muse for our designs. Our mission is to create a{" "}
+            <span className="highlight">brand identity</span> that is both
+            unique and memorable. We aim to reflect our company's values and
+            mission while also highlighting the connection to the location's
+            colors and textures.
+          </p>
+          <p>
+            To create a successful{" "}
+            <span className="highlight">branding strategy</span>, we consider
+            several elements. We strive to understand our{" "}
+            <span className="highlight">target audience</span> and what they
+            value. This helps us create a brand identity that resonates with
+            them. We identify and communicate our{" "}
+            <span className="highlight">unique selling proposition</span>{" "}
+            effectively. Finally, we ensure that our brand is{" "}
+            <span className="highlight">consistent</span> across all channels,
+            including social media, website, advertising, and packaging.
+          </p>
+        </div>
+        <div className="separator"></div>
+        <div className="section">
+          <h2>Identity</h2>
+          <p>
+            Our <span className="highlight">brand identity</span> evokes a sense
+            of authenticity and simplicity while highlighting the connection to
+            the earthy colors of the Dallol Depression. Our logo is simple yet
+            memorable, with a color palette that reflects the location's
+            distinct hues. Our tagline, "Elevate your space with our
+            earth-inspired designs," communicates our brand's mission and values
+            while also highlighting its connection to the location.
+          </p>
+          <p>
+            By incorporating these elements into our{" "}
+            <span className="highlight">branding strategy</span>, Dallol
+            differentiates itself from competitors and attracts customers who
+            value
+            <span className="highlight">
+              {" "}
+              authenticity, durability, and unique design
+            </span>
+            .
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Catalog/Catalog.jsx
+++ src/Components/Catalog/Catalog.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import "./Catalog.css";
+import "../../Img/C1.jpg";
+import "../../Img/C2.jpg";
+import "../../Img/C3.jpg";
+import "../../Img/C4.jpg";
+import "../../Img/C5.jpg";
+import "../../Img/C6.jpg";
+import "../../Img/C7.jpg";
 
-const Catalog = () => {
-  return <div id="catalog">Catalog</div>;
-};
+const images = [
+  { url: "../../Img/C1.jpg", title: "title1" },
+  { url: "../../Img/C2.jpg", title: "title2" },
+  { url: "../../Img/C3.jpg", title: "title2" },
+  { url: "../../Img/C4.jpg", title: "title2" },
+  { url: "../../Img/C5.jpg", title: "title2" },
+  { url: "../../Img/C6.jpg", title: "title2" },
+  { url: "../../Img/C7.jpg", title: "title2" },
+];
+
+function Catalog() {
+  return (
+    <div className="catalog">
+      {images.map((image, index) => (
+        <div className="catalog-item" key={index}>
+          <img src={image.url} alt={image.title} className="catalog-image" />
+          <div className="catalog-title">{image.title}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default Catalog;
